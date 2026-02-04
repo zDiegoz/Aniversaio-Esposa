faebbe0f6389df699765e44c93ade0ff9ae834c8
Diego <diegowilliam096@gmail.com>
2026-02-04 10:40:01 -0300

    Commit 2

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const btn = document.getElementById('btnSurpresa');
 const msg = document.getElementById('mensagem');
 const musica = document.getElementById('musica');
+const container = document.querySelector('.container');
 
 btn.addEventListener('click', () => {
 
+    // Mostra mensagem
     msg.style.display = 'block';
 
+    // Centraliza tudo novamente
+    container.style.display = 'flex';
+    container.style.flexDirection = 'column';
+    container.style.alignItems = 'center';
+    container.style.justifyContent = 'center';
+
+    // Texto botão
     btn.innerText = 'Leia ❤️';
 
+    // Música
     musica.currentTime = 0;
     musica.play();
 });
